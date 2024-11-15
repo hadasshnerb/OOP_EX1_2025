694fd5b4d8241834f58be5d67bf0e2531230b26c
unknown <hadasraviv1@gmail.com>
2024-11-15 14:28:50 +0300

    add test file

---
+++ GameLogicTest.java
@@ -0,0 +1,52 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameLogicTest {
+
+    @org.junit.jupiter.api.Test
+    void locate_disc() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDiscAtPosition() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBoardSize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void validMoves() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFirstPlayer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSecondPlayer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setPlayers() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isFirstPlayerTurn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isGameFinished() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void reset() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void undoLastMove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void countFlips() {
+    }
+}
